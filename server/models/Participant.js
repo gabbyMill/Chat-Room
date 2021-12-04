1e4e13e7779c25405ecc66fea7f3f575e799fb98
Ido Strauss <strauss02@hotmail.co.il>
2021-12-04 17:37:38 +0200

    refactor naming

--- server/models/Participant.js
+++ server/models/Participant.js
@@ -1,18 +1,18 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 
 const participantSchema = mongoose.Schema({
   username: String,
-  text: String, // ? needed ?
-});
+  // text: String, // ? needed ?
+})
 
-const Participant = mongoose.model("Participant", participantSchema);
+const Participant = mongoose.model('Participant', participantSchema)
 
-participantSchema.set("toJSON", {
+participantSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-export default Participant;
+export default Participant
--- server/routes/loginRoute.js
+++ server/routes/loginRoute.js
@@ -1,26 +1,26 @@
-import express from "express";
-import Participant from "../models/Participant.js";
-const router = express.Router();
+import express from 'express'
+import Participant from '../models/Participant.js'
+const router = express.Router()
 
-router.get("/:username", async (req, res, next) => {
+router.get('/:username', async (req, res, next) => {
   try {
     // This can also be a middleware authorization and not a route
-    const { username } = req.params;
-    if (!username) throw { status: 500, message: "Invalid Username" };
-    const allParticipants = await Participant.find({});
-    console.log(allParticipants);
-    const flag = allParticipants.every(
-      part => part.user.toLowerCase() !== username.toLowerCase()
-    );
-    console.log(flag);
-    if (!flag) throw { status: 403, message: "Name taken" };
-    await Participant.create({ username });
-    const answer = await Participant.find({});
-    res.json(answer);
+    const { username } = req.params
+    if (!username) throw { status: 401, message: 'Invalid Username' }
+    const allParticipants = await Participant.find({})
+    console.log(allParticipants)
+    const isUsernameTaken = allParticipants.some(
+      (participant) => participant.user.toLowerCase() === username.toLowerCase()
+    )
+    console.log(isUsernameTaken)
+    if (isUsernameTaken) throw { status: 409, message: 'Name taken' }
+    await Participant.create({ username })
+    const answer = await Participant.find({})
+    res.json(answer)
   } catch (err) {
-    res.json(err);
+    res.json(err)
     // next(err)
   }
-});
+})
 
-export default router;
+export default router
