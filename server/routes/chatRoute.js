1cb9727c2093da12176997e0a0353a03b0209578
gabbyMill <gabbymillatiner@gmail.com>
2021-12-05 15:07:09 +0200

    Token
    Added user token, sent by helperfunction, created in server-side

--- server/routes/chatRoute.js
+++ server/routes/chatRoute.js
@@ -1,48 +1,74 @@
-import express from 'express'
-import axios from 'axios'
-import Participant from '../models/Participant.js'
-
-// import { EventEmitter } from 'events'
-// const emitter = new EventEmitter()
+import express from "express";
+import axios from "axios";
+import Participant from "../models/Participant.js";
 
 // configure axios to reject promises only for status codes higher than 500
-axios.defaults.validateStatus = (status) => {
-  return status < 500
-}
-const router = express.Router()
+axios.defaults.validateStatus = status => {
+  return status < 500;
+};
+const router = express.Router();
 
-/* =========================== */
+/* ======================================================== */
+/* ======================================================== */
 
-const CONNECTED_USERS = Participant.find({ online: true })
+let CONNECTED_USERS;
+(async () => {
+  CONNECTED_USERS = await Participant.find({});
+  // CONNECTED_USERS = await Participant.find({ online: true });
+})();
 
 function sendMessageToAll(name, text, time) {
-  CONNECTED_USERS.forEach((connection) => {
-    const obj = { name, text, time: new Date() }
-    connection.res.write(`data: ${JSON.stringify(obj)} \n\n`)
-  })
+  CONNECTED_USERS.forEach(connection => {
+    const obj = { name, text, time: new Date() };
+    connection.res.write(`data: ${JSON.stringify(obj)} \n\n`);
+  });
 }
 
-router.post('/newmsg', (req, res, next) => {
-  const { message, state } = req.body
-  console.log(message, state)
-  if (!message && !state) return
-  sendMessageToAll(state, message)
-  res.end() // can edit this to return a value
-})
-
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
   res.writeHead(200, {
-    'Content-Type': 'text/event-stream',
-  })
-  const { inputValue } = 'guest' // username buld it fucker
-  CONNECTED_USERS.push({ username: inputValue, res })
+    "Content-Type": "text/event-stream",
+  });
+  const { inputValue } = "guest"; // username buld it fucker
+  if (!CONNECTED_USERS) return; // Otherwise, tries to push into a not yet initialized version of ConnectedUsers
+  if (!inputValue) return;
+  CONNECTED_USERS.push({ username: inputValue, res });
 
-  CONNECTED_USERS.forEach((item) => {
-    res.write(`data: ${inputValue} has now connected \n\n`)
-  })
-  req.on('close', () => {
+  CONNECTED_USERS.forEach(item => {
+    res.write(`data: ${inputValue} has now connected \n\n`);
+  });
+  req.on("close", () => {
     // Here you should add dissconnetion functionality, send a message to all other users
-  })
-})
+  });
+});
 
-export default router
+router.post("/newmsg", (req, res, next) => {
+  const { message, state } = req.body;
+  console.log(message, state);
+  if (!message && !state) return;
+  sendMessageToAll(state, message);
+  res.end(); // can edit this to return a value
+});
+
+router.get("/:username", async (req, res, next) => {
+  try {
+    // This can also be a middleware authorization and not a route
+    const { username } = req.params;
+    if (!username) throw { status: 401, message: "Invalid Username" };
+    const allParticipants = await Participant.find({});
+    console.log(allParticipants);
+    const isUsernameTaken = allParticipants.some(
+      participant => participant.user.toLowerCase() === username.toLowerCase()
+    );
+    console.log(isUsernameTaken);
+    if (isUsernameTaken) throw { status: 409, message: "Name taken" };
+    const userToken = jwt.sign(username, process.env.secretKey);
+    await Participant.create({ username, token: userToken, online: false });
+    // const answer = await Participant.find({})
+    res.json(userToken);
+  } catch (err) {
+    res.json(err);
+    // next(err)
+  }
+});
+
+export default router;
--- server/routes/loginRoute.js
+++ server/routes/loginRoute.js
@@ -1,28 +1,27 @@
-import express from 'express'
-import Participant from '../models/Participant.js'
-import jwt from 'jsonwebtoken'
-const router = express.Router()
+import express from "express";
+import Participant from "../models/Participant.js";
+import jwt from "jsonwebtoken";
+const router = express.Router();
 
-router.get('/:username', async (req, res, next) => {
+router.get("/:username", async (req, res, next) => {
+  console.log("WE MADE IT TO THE ROUTE");
   try {
     // This can also be a middleware authorization and not a route
-    const { username } = req.params
-    if (!username) throw { status: 401, message: 'Invalid Username' }
-    const allParticipants = await Participant.find({})
-    console.log(allParticipants)
+    const { username } = req.params;
+    if (!username) throw { status: 401, message: "Invalid Username" };
+    const allParticipants = await Participant.find({});
     const isUsernameTaken = allParticipants.some(
-      (participant) => participant.user.toLowerCase() === username.toLowerCase()
-    )
-    console.log(isUsernameTaken)
-    if (isUsernameTaken) throw { status: 409, message: 'Name taken' }
-    const userToken = jwt.sign(username, process.env.secretKey)
-    await Participant.create({ username, token: userToken, online: false })
-    // const answer = await Participant.find({})
-    res.json(userToken)
+      participant =>
+        participant.username.toLowerCase() === username.toLowerCase()
+    );
+    if (isUsernameTaken) throw { status: 409, message: "Name taken" };
+    const userToken = jwt.sign(username, process.env.secretKey);
+    await Participant.create({ username, token: userToken, online: false });
+    res.json(userToken);
   } catch (err) {
-    res.json(err)
+    res.json(err);
     // next(err)
   }
-})
+});
 
-export default router
+export default router;
